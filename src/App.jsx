200eceb87e102c591baf7efb886b9651bb3a6442
AnindayMondal <aninday.mondal@kreeti.com>
2025-08-21 14:24:24 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <marquee behavior="" direction="right">Notepad App coming Soon</marquee>
+    </>
+  )
+}
+
+export default App
